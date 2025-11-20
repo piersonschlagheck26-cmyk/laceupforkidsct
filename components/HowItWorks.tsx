da8d06f0828b4cee70cb9886ac7b81acf153c6aa
Pierson Schlagheck <piersonschlagheck@Piersons-MacBook-Pro.local>
2025-11-20 10:18:10 -0500

    Change all 'proceeds' references to 'net proceeds'

--- components/HowItWorks.tsx
+++ components/HowItWorks.tsx
@@ -34,9 +34,9 @@ import { useInView } from 'framer-motion'
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
         </svg>
       ),
-      title: 'Proceeds Donated',
+      title: 'Net Proceeds Donated',
     description:
-      '100% of the proceeds from shoe sales go directly to RMH CTMA, helping families stay close to their children during medical treatment.',
+      '100% of the net proceeds from shoe sales go directly to RMH CTMA, helping families stay close to their children during medical treatment.',
     },
   ]
 
